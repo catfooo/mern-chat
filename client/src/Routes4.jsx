d4598bd85baae34e6e6596d6228a58f720d4bdef
So Youn Choi <manghatuhyo@gmail.com>
2024-01-12 16:42:57 +0100

    problem starts here with not able to show logged in page when refreshes. so i tried to make sure that i have token, and decode it, and set it to get id and username, because when user refreshes, they dont have id and username value for this console log here. so still at same place, but tried something, and think need to go back? hope when i rethink i might think better than this..

---
+++ client/src/Routes4.jsx
@@ -0,0 +1,51 @@
+import { useContext, useEffect } from "react";
+import RegisterAndLoginForm from "./RegisterAndLoginForm";
+import { UserContext } from "./UserContext";
+// import jwt_decode from "jwt-decode"
+import { useState } from "react";
+import jwt from 'jsonwebtoken'
+
+const getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+};
+
+export default function Routes() {
+    const { username: contextUsername, id: contextId } = useContext(UserContext);
+    const [username, setUsername] = useState(contextUsername);
+    const [id, setId] = useState(contextId);
+
+    useEffect(() => {
+        setUsername(contextUsername);
+        setId(contextId);
+    }, [contextUsername, contextId]); // Only run when context values change
+
+
+    // const { username, id } = useContext(UserContext)
+    console.log('routes:', id,username)
+    const token = getCookie('token')
+    console.log('token in client:', token)
+    if (token) {
+        // const decodedToken = jwt_decode(token)
+        
+        // // Decode the base64-encoded payload manually
+        // const payloadBase64 = token.split('.')[1];
+        // const decodedPayload = atob(payloadBase64);
+        // const decodedToken = JSON.parse(decodedPayload);
+
+        const decodedToken = jwt.verify(token)
+
+
+        setUsername(decodedToken.username)
+        setId(decodedToken.id)
+    }
+
+    if (username) {
+        return 'logged in!' + username
+    }
+
+    return (
+        <RegisterAndLoginForm />
+    )
+}
