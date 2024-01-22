53097a334f388ce982ca68973af117a2d4cd0738
So Youn Choi <manghatuhyo@gmail.com>
2024-01-22 13:38:03 +0000

    uploading file. nend of video. next goal is... prob auth 0?

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 export default function RegisterAndLoginForm() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [isLoginOrRegister, setIsLoginOrRegister] = useState('register')
+    const [isLoginOrRegister, setIsLoginOrRegister] = useState('login')
     const {setUsername:setLoggedInUsername, setId} = useContext(UserContext)
     async function handleSubmit(ev) {
         ev.preventDefault()
@@ -36,7 +36,7 @@ export default function RegisterAndLoginForm() {
                     {isLoginOrRegister === 'register' && (
                         <div>
                             Already a member? 
-                            <button onClick={() => setIsLoginOrRegister('login')}>
+                            <button className="ml-1" onClick={() => setIsLoginOrRegister('login')}>
                               Login here
                             </button>
                         </div>
@@ -44,7 +44,7 @@ export default function RegisterAndLoginForm() {
                     {isLoginOrRegister === 'login' && (
                         <div>
                           Dont have an account?
-                          <button onClick={() => setIsLoginOrRegister('register')}>
+                          <button className="ml-1" onClick={() => setIsLoginOrRegister('register')}>
                             Register
                           </button>
                         </div>
